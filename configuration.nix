e83275876c6327ccb3b73f35c419e2ae36dcaa9b
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-02-05 16:33:45 +0000

    virtualisation done, added gaming packages

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,6 @@ let
 	user_complete_name = "Filipe Ligeiro Silva";
 
 	version = "22.11";
-	unstableTarball = fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
 in
 {
 	imports = [
@@ -19,15 +18,9 @@ in
 
 	nixpkgs.config = {
 		allowUnfree = true;
-		packageOverrides = pkgs: {
-			unstable = import unstableTarball {
-				config = config.nixpkgs.config;
-			};
-		};
 	};
 
 	boot = {
-		kernelPackages = pkgs.linuxPackages_latest;
 		loader.grub = {
 			enable = true;
 			version = 2;
@@ -41,13 +34,21 @@ in
 		hostName = "${host}";
 	};
 
-	programs = {
-		dconf.enable = true;
-		nm-applet.enable = true;
-		zsh = {
-			enable = true;
-			setOptions = [];
-		};
+	users.users.${user} = {
+		isNormalUser = true;
+		initialPassword = "${user}";
+		shell = pkgs.zsh;
+		description = "${user_complete_name}";
+		extraGroups = [
+			"audio"
+			"docker"
+			"libvirtd"
+			"networkmanager"
+			"storage"
+			"vboxusers"
+			"video"
+			"wheel"
+		];
 	};
 
 	time.timeZone = "Europe/Lisbon";
@@ -79,174 +80,21 @@ in
 	hardware = {
 		pulseaudio.enable = true;
 		bluetooth.enable = true;
+		opengl.driSupport32Bit = true;
 	};
 
-	services = {
-		blueman.enable = true;
-
-		xserver = {
+	programs = {
+		dconf.enable = true;
+		nm-applet.enable = true;
+		steam = {
 			enable = true;
-			libinput.enable = true;
-
-			desktopManager.xterm.enable = false;
-
-			displayManager = {
-				defaultSession = "none+i3";
-				# startx.enable = true; # TODO deploy with this line
-			};
-
-			windowManager.i3 = {
-				enable = true;
-				extraPackages = with pkgs; [ # {{{
-					# Display management
-					arandr
-					autorandr
-					brightnessctl
-					redshift
-					xdotool	# X11 automation tool
-
-					# Window manager
-					i3
-					i3status
-					i3lock
-					xss-lock
-					rofi
-
-					# Terminal emulator
-					alacritty
-
-					# Browser
-					firefox
-					chromium
-					tor-browser-bundle-bin
-
-					# Video/Audio management
-					vlc
-					mpv
-					clementine
-					yt-dlp
-					flac
-					sox
-					ffmpeg
-					handbrake
-					kid3
-					playerctl
-					obs-studio
-					spotify
-					streamlink  # Pipe streams into a video player
-					pavucontrol # Control audio sources/sinks
-
-					# Torrent management
-					transmission
-					transmission-gtk
-
-					# Image management
-					sxiv
-					feh
-					gthumb
-					exiftool
-					imagemagick
-					gimp
-					krita
-					inkscape
-					mediainfo
-					maim     # Screenshot utility
-					guvcview # Camera
-
-					# Theme management
-					arc-theme
-					lxappearance
-
-					# File management
-					xdg-user-dirs
-					xfce.thunar
-					xfce.thunar-archive-plugin
-					xfce.thunar-volman
-					xfce.tumbler
-					gnome.file-roller            # Archive manager for thunar
-					gvfs                         # Enables things like trashing files in Thunar
-					ntfs3g                       # Support for NTFS drives
-					lxde.lxsession               # This includes lxpolkit, in order to be able to mount some drives
-					perl536Packages.FileMimeInfo # Detect MIME type of files
-
-					# PDF management
-					pandoc
-					pdftk
-					libsForQt5.okular
-					zathura
-					ocamlPackages.cpdf
-					diff-pdf
-
-					# OCR
-					tesseract
-
-					# Clipboard management
-					xclip
-					xsel
-
-					# Other packages
-					discord
-					texlive.combined.scheme-full
-					libreoffice-still
-					tigervnc        # VNC server/client
-					remmina         # Remote desktop client
-					barrier         # KVM
-					bless           # Hex editor
-					gpick           # Color picker
-					mypaint         # Drawing table
-					scrcpy          # Android screen mirroring and control
-					vscode
-					onedrive        # OneDrive client
-					ventoy-bin-full # Make multiboot USB drives
-					xdragon         # Drag-and-drop source/sink
-					spek            # Audio inspector
-					# openasar # Make Discord faster TODO
-					]; # }}}
-			};
-
-			layout = "us";
-			xkbVariant = "altgr-intl";
-			xkbOptions = "ctrl:swapcaps";
+			remotePlay.openFirewall = true;
+			dedicatedServer.openFirewall = true;
 		};
-
-		openssh = {
+		zsh = {
 			enable = true;
-			passwordAuthentication = false;
-			kbdInteractiveAuthentication = false;
-			permitRootLogin = "no";
+			setOptions = [];
 		};
-	};
-
-	users.users.${user} = {
-		isNormalUser = true;
-		initialPassword = "${user}";
-		shell = pkgs.zsh;
-		description = "${user_complete_name}";
-		extraGroups = [
-			"audio"
-			"docker"
-			"libvirtd"
-			"networkmanager"
-			"storage"
-			"vboxusers"
-			"video"
-			"wheel"
-		];
-	};
-
-	fonts = {
-		fontDir.enable = true;
-		fonts = with pkgs; [
-			font-manager
-			iosevka
-			terminus_font
-			noto-fonts
-			noto-fonts-cjk
-			noto-fonts-emoji
-			corefonts # Microsoft fonts
-		];
-		# TODO add custom font
-		# fontconfig.localConf = builtins.readFile "/home/${user}/dotfiles/desktop/fontconfig/.config/fontconfig/fonts.conf";
 	};
 
 	environment.systemPackages = with pkgs; [ # {{{
@@ -455,36 +303,192 @@ in
 		openssh               # SSH programs
 
 		# Virtualisation
-		# docker
-		# docker-compose
-		# libvirt
-		# virt-manager
-		# virtualbox
-		# vagrant
+		docker
+		docker-compose
+		libvirt
+		virt-manager
+		virtualbox
+		vagrant
+
+		# Gaming
+		steam
+		lutris
+		heroic
 	]; # }}}
 
-	#virtualisation = {
-	#	docker = {
-	#		enable = true;
-	#		rootless = {
-	#			enable = true;
-	#			setSocketVariable = true;
-	#		};
-	#	};
-	#	libvirtd = {
-	#		enable = true;
-	#	};
-	#	virtualbox = {
-	#		host = {
-	#			enable = true;
-	#			enableExtensionPack = false;
-	#		};
-	#		guest = {
-	#			enable = true;
-	#			x11 = true;
-	#		};
-	#	};
-	#};
+	services = {
+		blueman.enable = true;
+
+		xserver = {
+			enable = true;
+			libinput.enable = true;
+
+			desktopManager.xterm.enable = false;
+
+			displayManager = {
+				defaultSession = "none+i3";
+				# startx.enable = true; # TODO deploy with this line
+			};
+
+			windowManager.i3 = {
+				enable = true;
+				extraPackages = with pkgs; [ # {{{
+					# Display management
+					arandr
+					autorandr
+					brightnessctl
+					redshift
+					xdotool	# X11 automation tool
+
+					# Window manager
+					i3
+					i3status
+					i3lock
+					xss-lock
+					rofi
+
+					# Terminal emulator
+					alacritty
+
+					# Browser
+					firefox
+					chromium
+					tor-browser-bundle-bin
+
+					# Video/Audio management
+					vlc
+					mpv
+					clementine
+					yt-dlp
+					flac
+					sox
+					ffmpeg
+					handbrake
+					kid3
+					playerctl
+					obs-studio
+					spotify
+					streamlink  # Pipe streams into a video player
+					pavucontrol # Control audio sources/sinks
+
+					# Torrent management
+					transmission
+					transmission-gtk
+
+					# Image management
+					sxiv
+					feh
+					gthumb
+					exiftool
+					imagemagick
+					gimp
+					krita
+					inkscape
+					mediainfo
+					maim     # Screenshot utility
+					guvcview # Camera
+
+					# Theme management
+					arc-theme
+					lxappearance
+
+					# File management
+					xdg-user-dirs
+					xfce.thunar
+					xfce.thunar-archive-plugin
+					xfce.thunar-volman
+					xfce.tumbler
+					gnome.file-roller            # Archive manager for thunar
+					gvfs                         # Enables things like trashing files in Thunar
+					ntfs3g                       # Support for NTFS drives
+					lxde.lxsession               # This includes lxpolkit, in order to be able to mount some drives
+					perl536Packages.FileMimeInfo # Detect MIME type of files
+
+					# PDF management
+					pandoc
+					pdftk
+					libsForQt5.okular
+					zathura
+					ocamlPackages.cpdf
+					diff-pdf
+
+					# OCR
+					tesseract
+
+					# Clipboard management
+					xclip
+					xsel
+
+					# Other packages
+					discord
+					texlive.combined.scheme-full
+					libreoffice-still
+					tigervnc        # VNC server/client
+					remmina         # Remote desktop client
+					barrier         # KVM
+					bless           # Hex editor
+					gpick           # Color picker
+					mypaint         # Drawing table
+					scrcpy          # Android screen mirroring and control
+					vscode
+					onedrive        # OneDrive client
+					ventoy-bin-full # Make multiboot USB drives
+					xdragon         # Drag-and-drop source/sink
+					spek            # Audio inspector
+					# openasar # Make Discord faster TODO
+					]; # }}}
+			};
+
+			layout = "us";
+			xkbVariant = "altgr-intl";
+			xkbOptions = "ctrl:swapcaps";
+		};
+
+		openssh = {
+			enable = true;
+			passwordAuthentication = false;
+			kbdInteractiveAuthentication = false;
+			permitRootLogin = "no";
+		};
+	};
+
+	fonts = {
+		fontDir.enable = true;
+		fonts = with pkgs; [
+			font-manager
+			iosevka
+			terminus_font
+			noto-fonts
+			noto-fonts-cjk
+			noto-fonts-emoji
+			corefonts # Microsoft fonts
+		];
+		# TODO add custom font
+		# fontconfig.localConf = builtins.readFile "/home/${user}/dotfiles/desktop/fontconfig/.config/fontconfig/fonts.conf";
+	};
+
+	virtualisation = {
+		docker = {
+			enable = true;
+			rootless = {
+				enable = true;
+				setSocketVariable = true;
+			};
+		};
+		libvirtd = {
+			enable = true;
+		};
+		virtualbox = {
+			host = {
+				enable = true;
+				enableExtensionPack = false;
+			};
+			guest = {
+				enable = true;
+				x11 = true;
+			};
+		};
+	};
 
 	system = {
 		autoUpgrade.enable = true;
