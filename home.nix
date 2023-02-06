1da235b3ed585d381f122681860841a31b8a540a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-02-06 13:45:33 +0000

    no readonly symlinks, don't want that

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 with builtins;
 {
 	home.username = "filipe";
@@ -9,19 +9,19 @@ with builtins;
 	programs.home-manager.enable = true;
 
 	home.file = {
-		".gitconfig".source = "/home/filipe/dotfiles/headless/git/.gitconfig";
-		".config/nvim".source = "/home/filipe/dotfiles/headless/nvim/.config/nvim";
-		".inputrc".source = "/home/filipe/dotfiles/headless/readline/.inputrc";
-		".screenrc".source = "/home/filipe/dotfiles/headless/screen/.screenrc";
-		".tmux.conf".source = "/home/filipe/dotfiles/headless/tmux/.tmux.conf";
-		".vimrc".source = "/home/filipe/dotfiles/headless/vim/.vimrc";
-		".zshrc".source = "/home/filipe/dotfiles/headless/zsh/.zshrc";
+		".gitconfig".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/git/.gitconfig";
+		".config/nvim".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/nvim/.config/nvim";
+		".inputrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/readline/.inputrc";
+		".screenrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/screen/.screenrc";
+		".tmux.conf".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/tmux/.tmux.conf";
+		".vimrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/vim/.vimrc";
+		".zshrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/zsh/.zshrc";
 
-		".config/alacritty/alacritty.yml".source = "/home/filipe/dotfiles/desktop/alacritty/.config/alacritty/alacritty.yml";
-		".config/i3".source = "/home/filipe/dotfiles/desktop/i3/.config/i3";
-		".config/i3status".source = "/home/filipe/dotfiles/desktop/i3/.config/i3status";
-		".config/sxiv".source = "/home/filipe/dotfiles/desktop/sxiv/.config/sxiv";
-		".Xresources".source = "/home/filipe/dotfiles/desktop/xresources/.Xresources";
-		".config/zathura/zathurarc".source = "/home/filipe/dotfiles/desktop/zathura/.config/zathura/zathurarc";
+		".config/alacritty/alacritty.yml".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/alacritty/.config/alacritty/alacritty.yml";
+		".config/i3".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/i3/.config/i3";
+		".config/i3status".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/i3/.config/i3status";
+		".config/sxiv".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/sxiv/.config/sxiv";
+		".Xresources".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/xresources/.Xresources";
+		".config/zathura/zathurarc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/zathura/.config/zathura/zathurarc";
 	};
 }
