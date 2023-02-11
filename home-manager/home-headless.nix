1c002966dce96cc91c387a1edef164d6c96a522c
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-02-11 23:28:00 +0000

    .

--- home-manager/home-headless.nix
+++ home-manager/home-headless.nix
@@ -8,7 +8,7 @@
 		enable = true;
 		plugins = [
 			{
-				name = "forgit";
+				name = "git-forgit";
 				src = "${pkgs.zsh-forgit}/share/zsh/zsh-forgit";
 				file = "${pkgs.zsh-forgit}/share/zsh/zsh-forgit/forgit.plugin.zsh";
 			}
