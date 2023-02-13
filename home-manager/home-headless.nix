ac9a6d833a5a330327139e983307a9d51cdff2ed
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-02-13 19:39:59 +0000

    more changes

--- home-manager/home-headless.nix
+++ home-manager/home-headless.nix
@@ -3,26 +3,32 @@
 	home.username = "filipe";
 	home.homeDirectory = "/home/filipe";
 
+	home.stateVersion = "22.11";
+
+	programs.home-manager.enable = true;
+
+	programs.zsh = {
+		enable = true;
+		plugins = [
+			{
+				name = "forgit";
+				src = "${pkgs.zsh-forgit}/share/zsh/zsh-forgit";
+			}
+		];
+	};
+
 	home.file = {
+		".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/nvim/.config/nvim";
+		".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/git/.gitconfig";
+		".inputrc".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/readline/.inputrc";
+		".screenrc".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/screen/.screenrc";
+		".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/tmux/.tmux.conf";
+		".vimrc".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/vim/.vimrc";
+		".zshrc".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/zsh/.zshrc";
+
 		".config/tealdeer/config.toml".text = ''
 			[updates]
 			auto_update = true
 		'';
-
-		".gitconfig".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/git/.gitconfig";
-		".config/nvim".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/nvim/.config/nvim";
-		".inputrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/readline/.inputrc";
-		".screenrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/screen/.screenrc";
-		".tmux.conf".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/tmux/.tmux.conf";
-		".vimrc".source =  config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/headless/vim/.vimrc";
-		".zshrc" = {
-			onChange = "source $HOME/.zshrc";
-			text = builtins.readFile "/home/filipe/dotfiles/headless/zsh/.zshrc" +
-				''source "${pkgs.zsh-forgit}/share/zsh/zsh-forgit/forgit.plugin.zsh"'';
-		};
 	};
-
-	home.stateVersion = "22.11";
-
-	programs.home-manager.enable = true;
 }
