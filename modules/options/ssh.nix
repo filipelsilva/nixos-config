0449744a361f615f68b3f44410b609caefd8d53a
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-03-30 12:04:54 +0100

    added another key

--- modules/options/ssh.nix
+++ modules/options/ssh.nix
@@ -13,7 +13,13 @@
       };
       hostKeys = [
         {
-          comment = "host key";
+          comment = "host key (rsa)";
+          bits = 4096;
+          path = "/etc/ssh/ssh_host_rsa_key";
+          type = "rsa";
+        }
+        {
+          comment = "host key (ed25519)";
           path = "/etc/ssh/ssh_host_ed25519_key";
           rounds = 100;
           type = "ed25519";
