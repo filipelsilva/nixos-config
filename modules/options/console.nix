7f4d5776e40a81dcbeb4380a5975e9b006f858db
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-04-01 19:52:15 +0100

    testing more stuff

--- modules/options/console.nix
+++ modules/options/console.nix
@@ -7,6 +7,5 @@
   console = {
     keyMap = "us";
     font = "${pkgs.terminus_font}/share/consolefonts/ter-v20b.psf.gz";
-    # font = "Lat2-Terminus16"; # TODO pode não funcionar
   };
 }
--- users/filipe/graphical.nix
+++ users/filipe/graphical.nix
@@ -19,11 +19,11 @@
       sha256 = "31795d48aa6ee1b9d482bef954e44a86d8c6a5bd962cddb3736c3a3d530769ca";
     };
 
-    ".config/alacritty/alacritty.yml".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/alacritty/.config/alacritty/alacritty.yml";
-    ".config/i3".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/i3/.config/i3";
-    ".config/i3status".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/i3/.config/i3status";
-    ".config/sxiv".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/sxiv/.config/sxiv";
-    ".Xresources".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/xresources/.Xresources";
-    ".config/zathura/zathurarc".source = config.lib.file.mkOutOfStoreSymlink "/home/filipe/dotfiles/desktop/zathura/.config/zathura/zathurarc";
+    ".config/alacritty/alacritty.yml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/alacritty/.config/alacritty/alacritty.yml";
+    ".config/i3".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/i3/.config/i3";
+    ".config/i3status".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/i3/.config/i3status";
+    ".config/sxiv".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/sxiv/.config/sxiv";
+    ".Xresources".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/xresources/.Xresources";
+    ".config/zathura/zathurarc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/desktop/zathura/.config/zathura/zathurarc";
   };
 }
