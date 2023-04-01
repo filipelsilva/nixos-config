da57b556eaffaac515892d6a00dc0f93201f76ce
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-04-01 19:48:49 +0100

    testing font

--- modules/options/console.nix
+++ modules/options/console.nix
@@ -1,11 +1,12 @@
 {
   config,
+  pkgs,
   inputs,
   ...
 }: {
   console = {
     keyMap = "us";
-    # font = "${pkgs.terminus_font}/share/consolefonts/ter-v20b.psf.gz";
-    font = "Lat2-Terminus16"; # TODO pode não funcionar
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v20b.psf.gz";
+    # font = "Lat2-Terminus16"; # TODO pode não funcionar
   };
 }
