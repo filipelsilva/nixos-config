70b47ab208eeaaf962f3816d294acc03ccf4d17f
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-04-21 16:57:03 +0100

    added lsp packages to neovim

--- modules/options/editor/headless.nix
+++ modules/options/editor/headless.nix
@@ -3,34 +3,60 @@
   pkgs,
   inputs,
   ...
-}: {
-  environment.systemPackages = with pkgs; [
-    ed
-    gnused
-    sd
-    vimHugeX
-    bvi
-    neovim
-    bless # Hex editor
-
-    # Pagers
-    less
-    lesspipe
-    bat
-    hexyl
-
-    # Finders
-    fzf
-    fd
-    silver-searcher
-    pdfgrep
-    ripgrep
-    ripgrep-all
-
-    # Run commands when files change
-    entr
-    watchexec
+}: let
+  neovim_lsp_packages = with pkgs; [
+    nodePackages_latest.bash-language-server
+    clang-tools
+    nodePackages_latest.dockerfile-language-server-nodejs
+    nodePackages_latest.vscode-langservers-extracted
+    gopls
+    jdt-language-server
+    lua-language-server
+    nodePackages_latest.pyright
+    rnix-lsp
+    rust-analyzer
+    terraform-ls
+    texlab
+    nodePackages_latest.typescript-language-server
+    nodePackages_latest.vim-language-server
   ];
+in {
+  environment.systemPackages = with pkgs;
+    [
+      ed
+      gnused
+      sd
+      vimHugeX
+      neovim
+
+      # (pkgs.buildFHSUserEnv {
+      #   name = "nvim";
+      #   targetPkgs = pkgs: with pkgs; [neovim];
+      #   runScript = "nvim";
+      # })
+
+      bvi
+      bless # Hex editor
+
+      # Pagers
+      less
+      lesspipe
+      bat
+      hexyl
+
+      # Finders
+      fzf
+      fd
+      silver-searcher
+      pdfgrep
+      ripgrep
+      ripgrep-all
+
+      # Run commands when files change
+      entr
+      watchexec
+    ]
+    ++ neovim_lsp_packages;
 
   services = {
     rsyncd.enable = true;
