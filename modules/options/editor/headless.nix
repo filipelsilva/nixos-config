59a9d70cf796f20863c9609b113ac20b0e0ec5b2
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-05-19 15:38:30 +0100

    added neovim lsps by hand

--- modules/options/editor/headless.nix
+++ modules/options/editor/headless.nix
@@ -3,38 +3,58 @@
   pkgs,
   inputs,
   ...
-}: {
-  environment.systemPackages = with pkgs; [
-    ed
-    gnused
-    sd
-    vim
-    neovim
-
-    bvi
-    bless # Hex editor
-
-    # Pagers
-    less
-    lesspipe
-    bat
-    hexyl
-
-    # Finders
-    fzf
-    fd
-    silver-searcher
-    pdfgrep
-    ripgrep
-    ripgrep-all
-
-    # Run commands when files change
-    entr
-    watchexec
-
-    # Convert files to UNIX format
-    dos2unix
+}: let
+  jdtlsWrapper = pkgs.writeShellScriptBin "jdtls" "jdt-language-server \"$@\"";
+  neovim_lsp_packages = with pkgs; [
+    nodePackages_latest.bash-language-server
+    clang-tools
+    nodePackages_latest.dockerfile-language-server-nodejs
+    nodePackages_latest.vscode-langservers-extracted
+    gopls
+    jdt-language-server
+    lua-language-server
+    nodePackages_latest.pyright
+    nil
+    rust-analyzer
+    texlab
+    nodePackages_latest.typescript-language-server
+    nodePackages_latest.vim-language-server
   ];
+in {
+  environment.systemPackages =
+    [jdtlsWrapper]
+    ++ (with pkgs; [
+      ed
+      gnused
+      sd
+      vim
+      neovim
+
+      bvi
+      bless # Hex editor
+
+      # Pagers
+      less
+      lesspipe
+      bat
+      hexyl
+
+      # Finders
+      fzf
+      fd
+      silver-searcher
+      pdfgrep
+      ripgrep
+      ripgrep-all
+
+      # Run commands when files change
+      entr
+      watchexec
+
+      # Convert files to UNIX format
+      dos2unix
+    ])
+    ++ neovim_lsp_packages;
 
   services = {
     rsyncd.enable = true;
