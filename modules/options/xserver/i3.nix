d20fd05c52d1611aebe907db41a07d5defa7422d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-06-27 22:39:01 +0100

    minor cleanup

--- modules/options/xserver/i3.nix
+++ modules/options/xserver/i3.nix
@@ -44,14 +44,13 @@
           ${pkgs.xorg.xset}/bin/xset -b s off -dpms
           ${pkgs.feh}/bin/feh --bg-fill ~/.background-image
           ${pkgs.lxde.lxsession}/bin/lxpolkit &
-          ${lib.optionalString config.services.autorandr.enable "${pkgs.autorandr}/bin/autorandr --change --skip-options crtc"}
           ${lib.optionalString config.services.xserver.displayManager.lightdm.enable "${pkgs.lightlocker}/bin/light-locker &"}
+          ${lib.optionalString config.services.autorandr.enable "${pkgs.autorandr}/bin/autorandr --change --skip-options crtc"}
           ${lib.optionalString config.programs.thunar.enable "${pkgs.xfce.thunar}/bin/thunar --daemon &"}
           ${lib.optionalString config.networking.networkmanager.enable "${pkgs.networkmanagerapplet}/bin/nm-applet &"}
           ${lib.optionalString config.hardware.bluetooth.enable "${pkgs.blueman}/bin/blueman-applet &"}
         '';
         extraPackages = with pkgs; [
-          lightlocker
           i3status
           rofi
         ];
