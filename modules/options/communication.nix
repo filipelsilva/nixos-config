daa84401e198f0855749077d9a3cfa8ae1ffe83d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-08-02 22:02:09 +0100

    Updated discord to not use openasar

--- modules/options/communication.nix
+++ modules/options/communication.nix
@@ -8,12 +8,4 @@
     zoom-us
     discord
   ];
-
-  nixpkgs.overlays = let
-    discordOverlay = self: super: {
-      discord = super.discord.override {
-        withOpenASAR = true;
-      };
-    };
-  in [discordOverlay];
 }
