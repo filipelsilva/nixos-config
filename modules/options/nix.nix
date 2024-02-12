64b166b5b398afdfe942ec4bf4097faa0532f7fe
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-02-12 00:47:04 +0000

    updated nix settings to auto clean

--- modules/options/nix.nix
+++ modules/options/nix.nix
@@ -1,18 +1,17 @@
-{pkgs, ...}: {
+{...}: {
   system.stateVersion = "23.11";
 
   nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      keep-outputs = true
-      keep-derivations = true
-    '';
-    gc.automatic = true;
     settings = {
+      experimental-features = [ "nix-command" "flakes" ];
       keep-outputs = true;
       keep-derivations = true;
       auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
   };
 
