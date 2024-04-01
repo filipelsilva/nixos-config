18013025226706f9ef93e53a1ed0624c98423213
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-04-01 08:26:12 +0100

    fix: steam with nvidia

--- modules/options/nvidia.nix
+++ modules/options/nvidia.nix
@@ -4,6 +4,13 @@
   lib,
   ...
 }: {
+  environment.sessionVariables = {
+    XDG_DATA_HOME = "$HOME/.local/share";
+  };
+  # Do this for steam TODO automate
+  # mkdir -p ~/.local/share/applications
+  # sed 's/^Exec=/&nvidia-offload /' /run/current-system/sw/share/applications/steam.desktop > ~/.local/share/applications/steam.desktop
+
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware = {
