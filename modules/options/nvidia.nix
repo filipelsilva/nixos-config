7f7ec37159cd8af4f5705ec4aec7fa970307585e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-05-25 01:38:10 +0100

    fix: moved to nvidia sync mode
    i'd rather be with offload, but I want to play games and not have my
    external monitor freeze on me, so there's that :D

--- modules/options/nvidia.nix
+++ modules/options/nvidia.nix
@@ -30,9 +30,10 @@
       };
       prime = {
         offload = {
-          enable = true;
+          enable = false;
           enableOffloadCmd = lib.mkIf config.hardware.nvidia.prime.offload.enable true;
         };
+        sync.enable = true;
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
