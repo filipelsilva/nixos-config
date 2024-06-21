0fa18bf1d2014a4d660ef43628e1037ceba56e92
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-06-21 22:36:31 +0100

    chore: another key to the mix

--- modules/options/git-server.nix
+++ modules/options/git-server.nix
@@ -11,10 +11,11 @@
       createHome = true;
       shell = "${pkgs.git}/bin/git-shell";
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFBS4VMQzOLdxYDSYuMb9FN7vbi2I7iUuyJ8Ei+piq3C pixel7a"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDO1MtviYyp8XTpV1i8PwiRkKu/4hmUQ9zWZM5UsLFG2 N100"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEq7vY6uC3fO/XRiu4H30I6wNBduHFfSmqWrguigrxap Y540"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjqkOHYodMjourMLlNaCJLCE6f8rzkguRd16YTUU164 T490"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKVYZ2IpPkbF3iXLP/iHcdad0e4AytHZjtLG/ZHTrrmf iPad"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFBS4VMQzOLdxYDSYuMb9FN7vbi2I7iUuyJ8Ei+piq3C pixel7a"
       ];
     };
   };
