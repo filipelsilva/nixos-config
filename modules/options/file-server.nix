2d8ec9d916f7ddf39d54aab23dfe758ac7d38c29
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-06-23 12:37:47 +0100

    fix: changed some things about the file server

--- modules/options/file-server.nix
+++ modules/options/file-server.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [dufs];
 
-  systemd.user.services.file-server = {
+  systemd.services.file-server = {
     description = "file server using dufs";
     wantedBy = ["default.target"];
     script = ''
-      ${pkgs.dufs}/bin/dufs -A --log-file /var/log/file-server.log
+      ${pkgs.dufs}/bin/dufs -A
     '';
   };
 }
