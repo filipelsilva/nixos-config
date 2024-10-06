183537972d62ce67c26129fb91c18372f751ca0e
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-10-06 19:43:59 +0100

    chore: removed firewall openings from the servarr stack

--- profiles/servarr.nix
+++ profiles/servarr.nix
@@ -2,23 +2,18 @@
   services = {
     radarr = {
       enable = true;
-      openFirewall = true;
     };
     sonarr = {
       enable = true;
-      openFirewall = true;
     };
     readarr = {
       enable = true;
-      openFirewall = true;
     };
     lidarr = {
       enable = true;
-      openFirewall = true;
     };
     prowlarr = {
       enable = true;
-      openFirewall = true;
     };
   };
 }
