3a875d28fd09d7113b43e517842fd75a001f2ef4
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2024-11-23 12:13:06 +0000

    chore: fixed intel graphics packages
    T490 doesn't like vaapiIntel updated, because... reasons

--- profiles/intel.nix
+++ profiles/intel.nix
@@ -1,13 +1,21 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   hardware.graphics = {
     enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      (intel-vaapi-driver.override {enableHybridCodec = true;}) # previously vaapiIntel
-      vaapiVdpau
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-      vpl-gpu-rt # QSV on 11th gen or newer
-      intel-media-sdk # QSV up to 11th gen
-    ];
+    extraPackages = with pkgs;
+      [
+        intel-media-driver
+        vaapiVdpau
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+        vpl-gpu-rt # QSV on 11th gen or newer
+        intel-media-sdk # QSV up to 11th gen
+      ]
+      ++ lib.optional (config.networking.hostName != "T490") (
+        intel-vaapi-driver.override {enableHybridCodec = true;} # previously vaapiIntel
+      );
   };
 }
