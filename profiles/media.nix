af486fa6a86fe7efa4b1179801cd80e1d152b349
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-03-02 22:45:35 +0000

    fix: dataPool caused errors for other devices

--- profiles/media.nix
+++ profiles/media.nix
@@ -1,31 +1,33 @@
 {
-  config,
   pkgs,
   lib,
   headless,
-  dataPool,
   user,
   ...
-}: {
+} @ inputs: let
+  hasDataPool = builtins.hasAttr "dataPool" inputs;
+  dataPool =
+    if hasDataPool
+    then inputs.dataPool
+    else {};
+in {
   nixpkgs.config.pulseaudio = true;
 
   services.transmission = {
     enable = true;
-    openRPCPort = true;
     inherit user;
     group = user;
-    settings =
-      {}
-      // lib.attrsets.optionalAttrs (config.networking.hostName == "N100") {
-        incomplete-dir-enabled = true;
-        incomplete-dir = "${dataPool.location}/torrents/.incomplete";
-        download-dir = "${dataPool.location}/torrents/Downloads";
-        rpc-bind-address = "0.0.0.0";
-      };
+    openRPCPort = hasDataPool;
+    settings = lib.attrsets.optionalAttrs hasDataPool {
+      incomplete-dir-enabled = true;
+      incomplete-dir = "${dataPool.location}/torrents/.incomplete";
+      download-dir = "${dataPool.location}/torrents/Downloads";
+      rpc-bind-address = "0.0.0.0";
+    };
     package = pkgs.transmission_4;
   };
 
-  systemd.services.transmission.serviceConfig.BindPaths = ["${dataPool.location}/torrents"];
+  systemd.services.transmission.serviceConfig.BindPaths = lib.lists.optionals hasDataPool ["${dataPool.location}/torrents"];
 
   environment.systemPackages = with pkgs;
     [
