b41b6cbe83d26d09fa60fbf5bb86d68e97d28e1d
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-07-04 09:41:44 +0100

    wip for docker containers

--- profiles/virtualisation/docker.nix
+++ profiles/virtualisation/docker.nix
@@ -17,4 +17,22 @@
   };
 
   hardware.nvidia-container-toolkit.enable = lib.mkIf (builtins.any (x: x == "nvidia") config.services.xserver.videoDrivers) true;
+
+  # virtualisation.oci-containers.containers = {
+  #   hackagecompare = {
+  #     image = "chrissound/hackagecomparestats-webserver:latest";
+  #     ports = ["127.0.0.1:3010:3010"];
+  #     volumes = [
+  #       "/root/hackagecompare/packageStatistics.json:/root/hackagecompare/packageStatistics.json"
+  #     ];
+  #     cmd = [
+  #       "--base-url"
+  #       "\"/hackagecompare\""
+  #     ];
+  #   };
+  # };
+
+  # https://www.reddit.com/r/immich/comments/1le3nco/immich_mediakit_a_smarter_way_to_manage_your/
+  # https://send.djazz.se/
+  # https://github.com/calibrain/calibre-web-automated-book-downloader
 }
