09839f1112d2ff1e1275609d9922571f3b727f64
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2025-12-01 00:14:20 +0000

    t490 setup

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -3,29 +3,10 @@
   lib,
   pkgs,
   ...
-}: let
+} @ inputs: let
   inherit (lib) types mkEnableOption mkOption mkIf;
 
   cfg = config.modules.wireguard;
-
-  keysFolder = "${config.userConfig.home}/.wireguard-keys";
-  generateWGKeys = let
-    wg = "${pkgs.wireguard-tools}/bin/wg";
-  in
-    pkgs.writeShellScript "generate-wg-keys" ''
-      mkdir -p ${keysFolder}
-      chown ${config.userConfig.name}:${config.userConfig.group} ${keysFolder}
-
-      privateKey="${keysFolder}/private"
-      publicKey="${keysFolder}/public"
-
-      if [ ! -f "$privateKey" ]; then
-        ${wg} genkey | tee "$privateKey" | ${wg} pubkey > "$publicKey"
-        chmod 600 "$privateKey"
-        chmod 644 "$publicKey"
-        chown ${config.userConfig.name}:${config.userConfig.group} "$privateKey" "$publicKey"
-      fi
-    '';
 in {
   options.modules.wireguard = {
     enable = mkEnableOption "wireguard";
@@ -88,67 +69,107 @@ in {
       }
     ];
 
-    system.activationScripts.generateWGKeys.text = "${generateWGKeys}";
+    # To create keys:
+    # umask 077
+    # wg genkey > privatekey
+    # wg pubkey < privatekey > publickey
 
-    networking = {
-      nat = {
-        enable = cfg.type == "server";
-        externalInterface = cfg.externalInterface;
-        internalInterfaces = ["wg0"];
-      };
-      firewall.allowedUDPPorts = [cfg.port];
-      wireguard.interfaces = {
-        wg0 = {
-          ips = ["${cfg.subnet}.${builtins.toString cfg.lastOctet}/24"];
-          listenPort = cfg.port;
-          postSetup =
-            if cfg.type == "server"
-            then ''
-              ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${cfg.subnet}.0/24 -o ${cfg.externalInterface} -j MASQUERADE
-            ''
-            else "";
-          postShutdown =
-            if cfg.type == "server"
-            then ''
-              ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${cfg.subnet}.0/24 -o ${cfg.externalInterface} -j MASQUERADE
-            ''
-            else "";
-          privateKeyFile = "${keysFolder}/private";
-          peers =
-            [
-              {
-                name = "N100";
-                publicKey = "HqdoDNKy6da1z6UyBrCt71U7ZgOPqCXuY966zVWFtjw=";
-                endpoint = "pipinhohome.hopto.org:${builtins.toString cfg.port}";
-                persistentKeepalive = 25;
-                allowedIPs = ["${cfg.subnet}.0/24"];
-              }
-              {
-                name = "Y540";
-                publicKey = "3PO5QzeOrYKzhhdI5tewfIHyxQB+k9SQSm0x0PrcZm8=";
-                endpoint = "ligeirosilva.hopto.org:${builtins.toString cfg.port}";
-                persistentKeepalive = 25;
-                allowedIPs = ["${cfg.subnet}.2/32"];
-              }
-            ]
-            ++ lib.lists.optionals (cfg.type == "server") [
-              {
-                name = "T490";
-                publicKey = "KsOJ59jkvpaRwNGHl5ccWJaP5pHKHlvdz18V451xRF4=";
-                allowedIPs = ["${cfg.subnet}.3/32"];
-              }
-              {
-                name = "iPad";
-                publicKey = "SYd35k2DSJ7LTwl/5UIIUzQCfVZTvVntF+NtvD94K2M=";
-                allowedIPs = ["${cfg.subnet}.4/32"];
-              }
-              {
-                name = "pixel7a";
-                publicKey = "ur16KiJ8BjKzLyrSzCqD3iWk26zcXXblkd1fxi6Onjg=";
-                allowedIPs = ["${cfg.subnet}.5/32"];
-              }
-            ];
+    environment.systemPackages = [pkgs.wireguard-tools];
+
+    age.secrets.wg-privatekey = {
+      file = "${inputs.self.outPath}/secrets/wg-privatekey-${config.networking.hostName}.age";
+      # for permission, see man systemd.netdev
+      mode = "640";
+      owner = "systemd-network";
+      group = "systemd-network";
+    };
+
+    networking.firewall = {
+      allowedUDPPorts = [cfg.port];
+      checkReversePath = mkIf (cfg.type == "client") "loose";
+    };
+
+    networking.nat = mkIf (cfg.type == "server") {
+      enable = true;
+      enableIPv6 = true;
+      externalInterface = cfg.externalInterface;
+      internalInterfaces = ["wg0"];
+    };
+
+    networking.useNetworkd = true;
+
+    systemd.network = {
+      enable = true;
+
+      networks."50-wg0" = {
+        matchConfig.Name = "wg0";
+
+        address = [
+          "${cfg.subnet}.${builtins.toString cfg.lastOctet}/32"
+        ];
+
+        networkConfig = mkIf (cfg.type == "server") {
+          # do not use IPMasquerade,
+          # unnecessary, causes problems with host ipv6
+          IPv4Forwarding = true;
+          IPv6Forwarding = true;
         };
+      };
+
+      netdevs."50-wg0" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "wg0";
+        };
+
+        wireguardConfig = {
+          ListenPort = cfg.port;
+
+          # Ensure file is readable by `systemd-network` user
+          PrivateKeyFile = config.age.secrets.wg-privatekey.path;
+
+          # Automatically create routes for AllowedIPs
+          RouteTable = "main";
+
+          # FirewallMark marks all packets send and received by wg0
+          # with the number 42, which can be used to define policy rules on these packets.
+          FirewallMark = 42;
+        };
+
+        wireguardPeers =
+          [
+            {
+              # N100 - Server
+              PublicKey = "HqdoDNKy6da1z6UyBrCt71U7ZgOPqCXuY966zVWFtjw=";
+              Endpoint = "pipinhohome.hopto.org:${builtins.toString cfg.port}";
+              PersistentKeepalive = 25;
+              AllowedIPs = ["${cfg.subnet}.1/32"];
+            }
+            {
+              # Y540 - Server
+              PublicKey = "3PO5QzeOrYKzhhdI5tewfIHyxQB+k9SQSm0x0PrcZm8=";
+              Endpoint = "ligeirosilva.hopto.org:${builtins.toString cfg.port}";
+              PersistentKeepalive = 25;
+              AllowedIPs = ["${cfg.subnet}.2/32"];
+            }
+          ]
+          ++ lib.lists.optionals (cfg.type == "server") [
+            {
+              # T490
+              PublicKey = "KsOJ59jkvpaRwNGHl5ccWJaP5pHKHlvdz18V451xRF4=";
+              AllowedIPs = ["${cfg.subnet}.3/32"];
+            }
+            {
+              # iPad
+              PublicKey = "SYd35k2DSJ7LTwl/5UIIUzQCfVZTvVntF+NtvD94K2M=";
+              AllowedIPs = ["${cfg.subnet}.4/32"];
+            }
+            {
+              # pixel7a
+              PublicKey = "ur16KiJ8BjKzLyrSzCqD3iWk26zcXXblkd1fxi6Onjg=";
+              AllowedIPs = ["${cfg.subnet}.5/32"];
+            }
+          ];
       };
     };
   };
