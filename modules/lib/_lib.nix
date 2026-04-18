16af8bab4ba0f4428519f005afa06c5398a8f5dd
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2026-04-18 16:47:12 +0100

    refactor: flatten user modules to single .nix files

--- modules/lib/_lib.nix
+++ modules/lib/_lib.nix
@@ -4,11 +4,13 @@
     path:
     let
       entries = builtins.readDir path;
-      files = lib.filterAttrs (n: t: t == "directory" && !lib.hasPrefix "_" n) entries;
+      files = lib.filterAttrs (
+        n: t: t == "regular" && lib.hasSuffix ".nix" n && !lib.hasPrefix "_" n
+      ) entries;
     in
     lib.mapAttrs' (n: _: {
-      name = n;
-      value = path + "/${n}/default.nix";
+      name = lib.removeSuffix ".nix" n;
+      value = path + "/${n}";
     }) files;
 
   forAllUsers = users: specFn: lib.mkMerge (map (user: { ${user} = specFn user; }) users);
